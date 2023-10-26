c11e622a62c30865031bd12b60f283c9fe5fc065
Adam <adamdang14@gmail.com>
2023-10-26 14:35:45 -0400

    final

--- frontend/src/screens/LandingPage/LandingPage.js
+++ frontend/src/screens/LandingPage/LandingPage.js
@@ -20,8 +20,8 @@ function LandingPage({ history }) {
         <Row>
           <div className="intro-text">
             <div>
-              <h1 className="title">Welcome to Note Zipper</h1>
-              <p className="subtitle">One Safe place for all your notes.</p>
+              <h1 className="title">Project Notes</h1>
+              <p className="subtitle">Get started getting it done.</p>
             </div>
             <div className="buttonContainer">
               <Link to="/login">
